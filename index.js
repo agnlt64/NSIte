f41df4d3104de6c224830c8953fd8eb2a423af25
ABFStudio <jaimelesfraisesbleues@gmail.com>
2023-01-27 21:22:14 +0100

    Fix file structure

--- index.js
+++
@@ -1,8 +0,0 @@
-var btn = document.getElementsByClassName("button");
-var maths = document.getElementsByClassName("Mathematiques");
-var physique = document.getElementsByClassName("Physique-Chimie");
-var Techno = document.getElementsByClassName("Technologie");
-
-function moyenne(){
-
-}
