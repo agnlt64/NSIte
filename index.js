84974cf9fff318148bb264840b8fecd4f708cd40
Rhyme2 <123723428+Rhyme2@users.noreply.github.com>
2023-01-27 21:11:06 +0100

    Add files via upload
    début moyenne

---
+++ index.js
@@ -0,0 +1,8 @@
+var btn = document.getElementsByClassName("button");
+var maths = document.getElementsByClassName("Mathematiques");
+var physique = document.getElementsByClassName("Physique-Chimie");
+var Techno = document.getElementsByClassName("Technologie");
+
+function moyenne(){
+
+}
