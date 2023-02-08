666b9015e5605b1a4045982b190a61f80dfb61ce
Antonin <jaimelesfraisesbleues@gmail.com>
2023-02-08 21:56:39 +0100

    add body variable

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def contact():
         name = request.form['name']
         email = request.form['email']
         subject = request.form['subject']
+        body = request.form['message']
         if email != "" and name != "" and subject != "":
             flash("Votre message a bien été envoyé !", category='success')
         else:
