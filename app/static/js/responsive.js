1169e13787ede54c76238e29463cbf8b4aacc4d9
Antonin GENELOT <Antonin@Antonin>
2023-02-13 19:04:57 +0100

    Everything is responsive

--- app/static/js/responsive.js
+++ app/static/js/responsive.js
@@ -1,6 +1,14 @@
 const hamburgerMenu = document.getElementById("hamburger-menu")
 const menu = document.getElementById("menu")
+menu.style.display = "none"
+const title = document.getElementById("title")
+const footers = document.getElementsByTagName("footer")
 
 hamburgerMenu.addEventListener("click", () => {
-    menu.style.display == "none" ? menu.style.display = "block" : menu.style.display = "none"
+    menu.style.display == "none" ? menu.style.display = "flex" : menu.style.display = "none"
+    menu.style.display == "flex" ? title.style.display = "none" : title.style.display = "flex"
 })
+
+for(f of footers) {
+    f.className = "footer"
+}
