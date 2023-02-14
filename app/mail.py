46dbc8894c02404e4320e144024ac36692efdb27
Antonin GENELOT <Antonin@Antonin>
2023-02-14 14:22:45 +0100

    fix security

--- app/mail.py
+++ app/mail.py
@@ -4,15 +4,12 @@ import datetime
 
 PORT = 587
 
-with open('password.txt', 'r') as file:
-    PASSWORD = file.read().encode('utf-8')
-
 def send_mail(name: str, receiver: str, subject: str, body: str) -> bool:
     current = datetime.datetime.now()
     message = f"Votre message a bien été envoyé. Voici un récapitulatif :\nVotre nom : {name}\nVotre message : \n{body}\n\nEnvoyé le {current.day}/{current.month}/{current.year} à {current.hour}:{current.minute}"
 
     try:
-        yag = yagmail.SMTP('jaimelesfraisesbleues@gmail.com', PASSWORD.decode('utf-8'))
+        yag = yagmail.SMTP('jaimelesfraisesbleues@gmail.com', "oxvdwydiiunywnou")
         yag.send(receiver, subject, message)
         return True
 
