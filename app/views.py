7a0f6abc18e79312f60e7daf11d3bf825e3b82e4
Antonin GENELOT <Antonin@Antonin>
2023-02-18 00:07:25 +0100

    Begin research field, not finished yet

--- app/views.py
+++ app/views.py
@@ -1,9 +1,19 @@
 from flask import Blueprint, render_template, flash, request
-import secrets
 from .mail import send_mail
+import secrets
+import requests
+import socket
+from bs4 import BeautifulSoup
 
 views = Blueprint('views', __name__)
 views.secret_key = secrets.token_urlsafe(16)
+
+PORT = 8080
+LOCAL_URL = f'http://{socket.gethostbyname(socket.gethostname())}:{PORT}/presentation/'
+
+def remove_html(text: str):
+    soup = BeautifulSoup(text, 'html.parser')
+    return soup.get_text()
 
 @views.route('/')
 def index():
@@ -30,3 +40,20 @@ def contrainte():
 @views.route('/presentation/')
 def presentation():
     return render_template('presentation.html')
+
+@views.route('/search-results/', methods=['GET', 'POST'])
+def search_results():
+    link = '/presentation/'
+    page = 'Présentation'
+    search_results = set()
+    if request.method == 'POST':
+        search = request.form['search']
+        presentation_page_response = requests.get(LOCAL_URL)
+        presentation_page_text = remove_html(presentation_page_response.text).split()
+        for word in presentation_page_text:
+            if search.lower() in word:
+                search_results.add(word)
+                # flash(f"{presentation_page_text[presentation_page_text.index(word) - 40]} {word} {presentation_page_text[presentation_page_text.index(word) + 40]}")
+        for search_result in search_results:
+            flash(f"{search_result}")
+    return render_template('search.html', link=link, page=page)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from app import create_app
+from app.views import PORT
 import socket
 
 app = create_app()
 hostname = socket.gethostname()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080, host=socket.gethostbyname(hostname))
+    app.run(debug=True, port=PORT, host=socket.gethostbyname(hostname))
