803464e8edee59a52aa462c86e72d50b393ae0d6
Antonin GENELOT <Antonin@Antonin>
2023-02-23 23:10:37 +0100

    continue search algo, todo: debug

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -5,16 +5,22 @@ function stripHTMLTags(str) {
     return str.replace(/<[^>]*>/g, '').split(' ').filter(Boolean);
 }
 
+const allSpans = document.getElementsByTagName('span')
 if (location.pathname === '/search-results/') {
-    // get all the ids of the returned keywords
-    let ids = new Array()
-    for (id of allKeywords) {
-        ids.push(id.innerHTML)
-        document.getElementById(id.innerHTML)
+    localStorage.clear()
+    // store all the ids of the returned keywords
+    for (let i = 0; i < allKeywords.length; i++) {
+        localStorage.setItem(`keyword${i}`, allKeywords[i].innerHTML)
     }
 } else if (location.pathname === '/') {
-    const allSpans = document.getElementsByTagName('span')
-    for (s of allSpans) {
-        console.log(s.id)
+    for (let i = 0; i < allSpans.length; i++) {
+        index = allSpans[i].innerHTML.split(' ').filter(Boolean)
+        let stored = localStorage.getItem(`keyword${i}`)
+        console.log(index.slice(0).includes(stored))
+        if (stored !== null) {
+            if (index.includes('spécialité')) {
+                allSpans[i].id = stored
+            }
+        }
     }
 }
