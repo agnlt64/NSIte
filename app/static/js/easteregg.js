5ce761cccc23c081b36ba0a6fdb08848b3bf4939
Antonin GENELOT <Antonin@Antonin>
2023-03-03 16:37:50 +0100

    finish responsive search bat

--- app/static/js/easteregg.js
+++ app/static/js/easteregg.js
@@ -1,13 +1,7 @@
-const easterEgg = document.getElementById('easter-egg')
 const search = document.getElementById('search')
-const rick = document.createElement('img')
-rick.src = '/static/media/rick.gif'
-rick.style.marginTop = '2rem'
-rick.style.width = '50rem'
-rick.style.height = '40rem'
 
 search.addEventListener('input', () => {
     if (search.value === 'rick roll') {
-        easterEgg.appendChild(rick)
+        document.body.style.backgroundImage = 'url("/static/media/rick.gif")'
     }
 })
